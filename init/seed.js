55d557d860921d1554ebbd41fb838d81e6961f5f
Sneha Yadav <143681944+ssyy21@users.noreply.github.com>
2026-05-19 13:23:26 +0530

    Update seed.js

--- init/seed.js
+++ init/seed.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const Listing = require("../models/listing");
 const initData = require("./data"); // contains dummyListings array
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 async function main() {
   try {
